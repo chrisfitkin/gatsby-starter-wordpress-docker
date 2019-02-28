ee5661ef0e6b2c9aa275073c91554a0e35c52dbc
Chris Fitkin <cfitkin@gmail.com>
2019-02-28 11:11:08 -0800

    Add Docker files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,63 @@
-module.exports = {
-  siteMetadata: {
-    title: 'Gatsby + WordPress Starter',
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
-    {
-      resolve: 'gatsby-source-wordpress',
-      options: {
-        // The base url to your WP site.
-        baseUrl: 'wpdemo.gatsbycentral.com',
-        // WP.com sites set to true, WP.org set to false
-        hostingWPCOM: false,
-        // The protocol. This can be http or https.
-        protocol: 'https',
-        // Use 'Advanced Custom Fields' Wordpress plugin
-        useACF: false,
-        auth: {},
-        // Set to true to debug endpoints on 'gatsby build'
-        verboseOutput: false,
-      },
+const config = {
+  dev: {
+    siteMetadata: {
+      title: 'MeetKite Gatsby Website',
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-purgecss',
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
-  ],
+    plugins: [
+      'gatsby-plugin-react-helmet',
+      'gatsby-plugin-sass',
+      {
+        resolve: 'gatsby-source-wordpress',
+        options: {
+          // The base url to your WP site.
+          baseUrl: 'wpdemo.gatsbycentral.com',
+          // WP.com sites set to true, WP.org set to false
+          hostingWPCOM: false,
+          // The protocol. This can be http or https.
+          protocol: 'https',
+          // Use 'Advanced Custom Fields' Wordpress plugin
+          useACF: false,
+          auth: {},
+          // Set to true to debug endpoints on 'gatsby build'
+          verboseOutput: false,
+        },
+      },
+      'gatsby-plugin-sharp',
+      'gatsby-transformer-sharp',
+      'gatsby-plugin-purgecss',
+      'gatsby-plugin-netlify', // make sure to keep it last in the array
+    ],
+  },
+  docker: {
+    plugins: [
+      'gatsby-plugin-react-helmet',
+      'gatsby-plugin-sass',
+      {
+        resolve: 'gatsby-source-wordpress',
+        options: {
+          // The base url to your WP site.
+          baseUrl: 'localhost:8001',
+          // WP.com sites set to true, WP.org set to false
+          hostingWPCOM: false,
+          // The protocol. This can be http or https.
+          protocol: 'http',
+          // Use 'Advanced Custom Fields' Wordpress plugin
+          useACF: false,
+          auth: {},
+          // Set to true to debug endpoints on 'gatsby build'
+          verboseOutput: false,
+        },
+      },
+      'gatsby-plugin-sharp',
+      'gatsby-transformer-sharp',
+      'gatsby-plugin-purgecss',
+      'gatsby-plugin-netlify', // make sure to keep it last in the array
+    ],
+  }
 }
+
+const nodeEnv = process.env.NODE_ENV || 'dev';
+module.exports = config[nodeEnv] ? {
+  ...config.dev,
+  ...config[nodeEnv]
+} : config.dev;
